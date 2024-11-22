ddc1ded9e39dab34a82c14e6055bfeb71fbe651c
dauntss <kat.krahulec@gmail.com>
2024-11-21 19:16:49 -0700

    updated DB to functional status

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,19 +6,25 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      ssl: true,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+  }}})
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'postgres',
+      ssl: true,
+    
       dialectOptions: {
         decimalNumbers: true,
       },
     });
-
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
-
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
 Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
-
 export { sequelize, User, Ticket };
